b564116d6e86d256d8c2ee84aaf8265bf5a6ede5
Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
2025-07-23 12:04:45 +0530

    chatqna-core: Initial integration of Ollama
    Signed-off-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>

--- sample-applications/chat-question-and-answer-core/app/utils.py
+++ sample-applications/chat-question-and-answer-core/app/utils.py
@@ -1,4 +1,7 @@
 import os
+import re
+import subprocess
+import time
 import openvino as ov
 import openvino.properties as props
 from .logger import logger
@@ -151,3 +154,55 @@ def get_device_property(device: str = ""):
             properties_dict[property_key] = property_val
 
     return properties_dict
+
+def download_ollama_model(model_id: str):
+
+    # Known limitation: Unset proxy environment variables to avoid issues with Ollama
+    os.environ.pop("HTTP_PROXY", None)
+    os.environ.pop("http_proxy", None)
+    os.environ.pop("https_proxy", None)
+
+    # Set the `OLLAMA_MODELS` to store the Ollama models
+    os.environ['OLLAMA_MODELS'] = f'/home/appuser/.ollama/models/{model_id}'
+
+    try:
+        serve_process = subprocess.Popen(["ollama", "serve"])
+
+        # Optional: wait a few seconds to ensure the server starts
+        time.sleep(5)
+
+        # Download the model using Ollama CLI
+        # pull_process = subprocess.run(["ollama", "pull", model_id], check=True)
+        pull_process = subprocess.run(
+            ["ollama", "pull", model_id],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        logger.info(f"Ollama model {model_id} downloaded successfully.")
+
+        # Run and load the model
+        run_process = subprocess.run(
+            ["ollama", "run", model_id],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        logger.info(f"Ollama model {model_id} is running successfully.")
+
+    except subprocess.CalledProcessError as e:
+        # Clean ANSI escape sequences and extract the last meaningful error line
+        raw_error = e.stderr or str(e)
+        clean_error = re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', '', raw_error) # Remove ANSI codes
+        lines = clean_error.strip().splitlines()
+        err_message = next((line for line in reversed(lines) if "Error:" in line), lines[-1] if lines else "Unknown error")
+        logger.error(f"Error downloading Ollama model {model_id}: {err_message}")
+        raise RuntimeError(f"Ollama failed: {err_message}") from e
+
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        raise e
